367b555c1ffc8e63d56821001d67763146c41cb2
League Student <league@iMac-13.attlocal.net>
2019-12-12 18:26:48 -0800

    left off on Nasty Suprise

--- src/_01_nasty_surprise/TrickorTreat.java
+++ src/_01_nasty_surprise/TrickorTreat.java
@@ -2,17 +2,24 @@ package _01_nasty_surprise;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TrickorTreat implements ActionListener {
-void gui() {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
+void gui() {
+	
 	frame.add(panel);
 	panel.add(button2);
 	panel.add(button1);
@@ -21,14 +28,28 @@ void gui() {
 	button1.addActionListener(this);
 	button2.addActionListener(this);
 
-}
 
+}
 @Override
-public void actionPerformed(ActionEvent e) {
+public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
-	if(e == "button1") {
+	JButton buttonPressed = (JButton) arg0.getSource();
+	if(button1 == buttonPressed) {
 		
 	}
 }
-
+private void showPictureFromTheInternet(String imageUrl) {
+    try {
+         URL url = new URL(imageUrl);
+         Icon icon = new ImageIcon(url);
+         JLabel imageLabel = new JLabel(icon);
+         JFrame frame = new JFrame();
+         frame.add(imageLabel);
+         frame.setVisible(true);
+         frame.pack();
+    } catch (MalformedURLException e) {
+         e.printStackTrace();
+    }
 }
+}
+//hi
